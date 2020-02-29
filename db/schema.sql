cc932e4b2e0b0a4ff74f475727e184ab2fe6b8bf
Zedekia Malawi <jatelo68@gmail.com>
2020-02-29 17:45:11 -0500

    updating

--- db/schema.sql
+++ db/schema.sql
@@ -1,11 +1,11 @@
-DROP DATABASE IF EXISTS burgers_db
+DROP DATABASE IF EXISTS Burgers_db
 
-CREATE DATABASE burgers_db;
-USE burgers_db;
+CREATE DATABASE Burgers_db;
+USE Burgers_db;
 
-CREATE TABLE burgers (
+CREATE TABLE Burgers (
     id INT NOT NULL AUTO_INCREMENT,
-    burger_name VARCHAR(255) NOT NULL,
+    Burger_name VARCHAR(255) NOT NULL,
     devoured BOOLEAN DEFAULT false,
     date TIMESTAMP,
     PRIMARY KEY (id)
